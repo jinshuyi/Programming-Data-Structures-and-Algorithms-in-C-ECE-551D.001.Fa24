1483396065a122a5072919ecc923074853f13024
sj445 <sj445@duke.edu>
2024-09-01 14:58:54 -0400

    Did assignment 14

---
+++ 013_comp_arith/mytypes.c
@@ -0,0 +1,10 @@
+// Complex number struct and typedef
+struct complex_num_tag {
+  double real_part;
+  double imaginary_part;
+};
+typedef struct complex_num_tag complex_num_t;
+
+// Animal enum and typedef
+enum animal_tag { CAT, FISH, SNAKE, DOG, HORSE };
+typedef enum animal_tag animal_t;
