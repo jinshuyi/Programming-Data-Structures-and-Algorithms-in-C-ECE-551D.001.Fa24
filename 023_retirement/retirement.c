95738328cbcd6b9e5abc820e28097b448a3e6fbc
sj445 <sj445@duke.edu>
2024-09-08 17:42:20 -0400

    did 23

--- 023_retirement/retirement.c
+++ 023_retirement/retirement.c
@@ -6,22 +6,22 @@ typedef struct _retire_info {
   double rate_of_return;
 } retire_info;
 
-void update_balance(int * ageInMonths, double * balance, retire_info info) {
+void update_balance(int ageInMonths, double balance, retire_info info) {
   for (int i = 0; i < info.months; i++) {
-    int ageYears = *ageInMonths / 12;
-    int ageMonths = *ageInMonths % 12;
+    int ageYears = ageInMonths / 12;
+    int ageMonths = ageInMonths % 12;
 
-    printf("Age %3d month %2d you have $%.2f\n", ageYears, ageMonths, *balance);
-    *balance += (*balance * info.rate_of_return) + info.contribution;
-    (*ageInMonths)++;
+    printf("Age %3d month %2d you have $%.2f\n", ageYears, ageMonths, balance);
+    balance += (balance * info.rate_of_return) + info.contribution;
+    (ageInMonths)++;
   }
 }
 void retirement(int startAge,           //in months
                 double initial,         //initial savings in dollars
                 retire_info working,    //info about working
                 retire_info retired) {  //info about being retired
-  update_balance(&startAge, &initial, working);
-  update_balance(&startAge, &initial, retired);
+  update_balance(startAge, initial, working);
+  update_balance(startAge, initial, retired);
 }
 
 int main() {
@@ -29,11 +29,8 @@ int main() {
 
   retire_info retired = {384, -4000, 0.01 / 12};
   int startAge = 327;
-  // Start at age 27 years and 3 months (327 months)
   double initialSavings = 21345;
-  // Initial savings of $21,345
 
-  // Calculate retirement plan
   retirement(startAge, initialSavings, working, retired);
 
   return 0;
