a5f2d7f0cd424c03ad08af401df90f3cdab58439
sj445 <sj445@duke.edu>
2024-09-08 18:17:16 -0400

    did 23

--- 023_retirement/retirement.c
+++ 023_retirement/retirement.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-typedef struct _retire_info {
+struct _retire_info {
   int months;
   double contribution;
   double rate_of_return;
-} retire_info;
+};
+
+typedef struct _retire_info retire_info;
 
 void update_balance(int ageInMonths, double balance, retire_info info) {
   for (int i = 0; i < info.months; i++) {
