55abb0cdbc140fdce127b729b5ec0f2c1b38532e
sj445 <sj445@duke.edu>
2024-09-30 14:28:09 -0400

    522

--- 052_rot_matrix/rotateMatrix.c
+++ 052_rot_matrix/rotateMatrix.c
@@ -1,58 +1,60 @@
-#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 #define MATRIX_SIZE 10
 
-void validateAndExtractMatrix(FILE * file, char matrix[MATRIX_SIZE][MATRIX_SIZE]);
-void printRotatedMatrix(char matrix[MATRIX_SIZE][MATRIX_SIZE]);
+void rotateMatrix(char matrix[MATRIX_SIZE][MATRIX_SIZE],
+                  char rotated[MATRIX_SIZE][MATRIX_SIZE]) {
+  for (int i = 0; i < MATRIX_SIZE; i++) {
+    for (int j = 0; j < MATRIX_SIZE; j++) {
+      rotated[j][MATRIX_SIZE - 1 - i] = matrix[i][j];
+    }
+  }
+}
 
-int main(int argc, char ** argv) {
+int main(int argc, char * argv[]) {
   if (argc != 2) {
-    fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
+    fprintf(stderr, "Usage: %s <input_file>\n", argv[0]);
     return EXIT_FAILURE;
   }
 
   FILE * file = fopen(argv[1], "r");
-  if (!file) {
-    perror("Cannot open the file");
+  if (file == NULL) {
+    perror("Error opening file");
     return EXIT_FAILURE;
   }
 
   char matrix[MATRIX_SIZE][MATRIX_SIZE];
-  validateAndExtractMatrix(file, matrix);
-  printRotatedMatrix(matrix);
+  char rotated[MATRIX_SIZE][MATRIX_SIZE];
+  char line[MATRIX_SIZE + 2];  // 10 characters + newline + null terminator
 
-  if (fclose(file) != 0) {
-    perror("Cannot close the file");
-    return EXIT_FAILURE;
+  for (int i = 0; i < MATRIX_SIZE; i++) {
+    if (fgets(line, sizeof(line), file) == NULL) {
+      fprintf(stderr, "Error reading line %d\n", i + 1);
+      fclose(file);
+      return EXIT_FAILURE;
+    }
+    if (strchr(line, '\n') == NULL || strlen(line) != MATRIX_SIZE + 1) {
+      fprintf(stderr, "Invalid line length at line %d\n", i + 1);
+      fclose(file);
+      return EXIT_FAILURE;
+    }
+    strncpy(matrix[i], line, MATRIX_SIZE);
+  }
+
+  fclose(file);
+
+  // Rotate the matrix
+  rotateMatrix(matrix, rotated);
+
+  // Print the rotated matrix
+  for (int i = 0; i < MATRIX_SIZE; i++) {
+    for (int j = 0; j < MATRIX_SIZE; j++) {
+      putchar(rotated[i][j]);
+    }
+    putchar('\n');
   }
 
   return EXIT_SUCCESS;
-}
-
-void validateAndExtractMatrix(FILE * file, char matrix[MATRIX_SIZE][MATRIX_SIZE]) {
-  char line[MATRIX_SIZE + 2];  // +2 for newline and null-terminator
-
-  for (int i = 0; i < MATRIX_SIZE; i++) {
-    if (fgets(line, sizeof(line), file) == NULL || line[0] == '\n' ||
-        line[MATRIX_SIZE] != '\n') {
-      fprintf(stderr, "Input file does not have a valid 10x10 matrix format.\n");
-      exit(EXIT_FAILURE);
-    }
-
-    for (int j = 0; j < MATRIX_SIZE; j++) {
-      matrix[j][MATRIX_SIZE - 1 - i] = line[j];
-    }
-  }
-}
-
-void printRotatedMatrix(char matrix[MATRIX_SIZE][MATRIX_SIZE]) {
-  for (int i = 0; i < MATRIX_SIZE; i++) {
-    for (int j = 0; j < MATRIX_SIZE; j++) {
-      printf("%c", matrix[i][j]);
-    }
-    printf("\n");
-  }
 }
