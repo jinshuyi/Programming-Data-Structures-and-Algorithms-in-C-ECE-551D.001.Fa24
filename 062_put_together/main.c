70eba65dfb823bd2279a50d3a7799f6777028064
sj445 <sj445@duke.edu>
2024-10-07 21:49:00 -0400

    62

--- 062_put_together/main.c
+++ 062_put_together/main.c
@@ -1,38 +1,74 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "kv.h"
+
 #include "counts.h"
+#include "kv.h"
 #include "outname.h"
+counts_t * countFile(const char * filename, kvarray_t * kvPairs);
+
+void handleError(const char * msg) {
+  perror(msg);
+  exit(EXIT_FAILURE);
+}
+
+FILE * safeOpenFile(const char * filename, const char * mode) {
+  FILE * f = fopen(filename, mode);
+  if (f == NULL) {
+    handleError("Cannot open file");
+  }
+  return f;
+}
+
+void safeCloseFile(FILE * f) {
+  if (fclose(f) != 0) {
+    handleError("Cannot close file");
+  }
+}
+
+void processFile(const char * filename, kvarray_t * kvPairs) {
+  counts_t * c = countFile(filename, kvPairs);
+  char * outName = computeOutputFileName(filename);
+
+  FILE * f = safeOpenFile(outName, "w");
+  printCounts(c, f);
+  safeCloseFile(f);
+
+  freeCounts(c);
+  free(outName);
+}
 
 counts_t * countFile(const char * filename, kvarray_t * kvPairs) {
-  //WRITE ME
-  return NULL;
+  FILE * f = safeOpenFile(filename, "r");
+  char * line = NULL;
+  size_t sz = 0;
+  counts_t * c = createCounts();
+
+  while (getline(&line, &sz, f) > 0) {
+    if (strchr(line, '\n') != NULL) {
+      *strchr(line, '\n') = '\0';
+    }
+    addCount(c, lookupValue(kvPairs, line));
+    free(line);
+    line = NULL;
+  }
+
+  free(line);
+  safeCloseFile(f);
+  return c;
 }
 
 int main(int argc, char ** argv) {
-  //WRITE ME (plus add appropriate error checking!)
- //read the key/value pairs from the file named by argv[1] (call the result kv)
+  if (argc <= 2) {
+    handleError("Not enough arguments");
+  }
 
- //count from 2 to argc (call the number you count i)
+  kvarray_t * kv = readKVs(argv[1]);
 
-    //count the values that appear in the file named by argv[i], using kv as the key/value pair
-    //   (call this result c)
+  for (int i = 2; i < argc; i++) {
+    processFile(argv[i], kv);
+  }
 
-    //compute the output file name from argv[i] (call this outName)
-
-
-    //open the file named by outName (call that f)
-
-    //print the counts from c into the FILE f
-
-    //close f
-
-    //free the memory for outName and c
-
-
-
- //free the memory for kv
-
+  freeKVs(kv);
   return EXIT_SUCCESS;
 }
