7dfcd739230ebf9c78d72a56d909e68e34df4458
sj445 <sj445@duke.edu>
2024-10-07 21:31:03 -0400

    60

--- 060_outname/outname.c
+++ 060_outname/outname.c
@@ -1,8 +1,19 @@
+#include "outname.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "outname.h"
 
 char * computeOutputFileName(const char * inputName) {
   //WRITE ME
+  size_t len = strlen(inputName);
+  char * outName = malloc(len + 8);  // for ".counts\0"
+
+  if (outName == NULL) {
+    return NULL;
+  }
+
+  strcpy(outName, inputName);
+  strcat(outName, ".counts");
+  return outName;
 }
