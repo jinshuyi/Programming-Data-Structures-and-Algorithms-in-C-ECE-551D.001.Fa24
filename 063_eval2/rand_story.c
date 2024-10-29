435b6b7cfe7f2ae5c86c91bb9e0e15e9d8656d35
sj445 <sj445@duke.edu>
2024-10-29 19:31:34 -0400

    63

--- 063_eval2/rand_story.c
+++ 063_eval2/rand_story.c
@@ -208,9 +208,6 @@ void replace_category_with_backreference(char * line,
   for (int i = 0; i < category_count; i++) {
     free(replacements[i]);
   }
-  for (int i = 0; i < used_count; i++) {
-    free(used_words[i]);
-  }
 }
 
 // 读取模板文件并处理回溯引用
