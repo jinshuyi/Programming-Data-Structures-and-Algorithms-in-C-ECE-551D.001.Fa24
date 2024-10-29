49a7d890af39c6bf3bd6173b4bcd3e3b3b99a78c
sj445 <sj445@duke.edu>
2024-10-28 20:51:49 -0400

    63

--- 063_eval2/story-step4.c
+++ 063_eval2/story-step4.c
@@ -5,40 +5,52 @@
 #include "rand_story.h"
 
 int main(int argc, char ** argv) {
-  if (argc < 3 || argc > 4) {
-    fprintf(stderr, "Usage: %s [-n] <words file> <story template file>\n", argv[0]);
-    exit(EXIT_FAILURE);
+  if (argc != 3 && argc != 4) {
+    fprintf(stderr, "Usage: %s [-n] words.txt story.txt\n", argv[0]);
+    return EXIT_FAILURE;
   }
 
-  int no_reuse = 0;
-  int arg_idx = 1;
-  if (strcmp(argv[arg_idx], "-n") == 0) {
-    no_reuse = 1;
-    arg_idx++;
+  int allow_repeat = 1;  // 默认允许重复
+  const char * words_filename;
+  const char * template_filename;
+
+  if (argc == 4 && strcmp(argv[1], "-n") == 0) {
+    allow_repeat = 0;  // 如果有 "-n"，则不允许重复
+    words_filename = argv[2];
+    template_filename = argv[3];
+  }
+  else if (argc == 3) {
+    words_filename = argv[1];
+    template_filename = argv[2];
+  }
+  else {
+    fprintf(stderr, "Invalid usage\n");
+    return EXIT_FAILURE;
   }
 
-  FILE * words_file = fopen(argv[arg_idx], "r");
-  if (words_file == NULL) {
-    perror("Could not open words file");
-    exit(EXIT_FAILURE);
+  // 打开单词文件
+  FILE * f = fopen(words_filename, "r");
+  if (f == NULL) {
+    perror("Could not open file");
+    return EXIT_FAILURE;
   }
 
-  catarray_t * cats = read_words(words_file);
-  fclose(words_file);
+  // 读取单词并存储在 cats 中
+  catarray_t * cats = readWords2(f);
+  fclose(f);
 
-  FILE * story_file = fopen(argv[arg_idx + 1], "r");
-  if (story_file == NULL) {
-    perror("Could not open story template file");
-    free_catarray(cats);
-    exit(EXIT_FAILURE);
+  // 初始化 used_words 用于引用跟踪
+  category_t used_words = {NULL, NULL, 0};
+
+  // 读取模板文件并替换类别标记，根据 allow_repeat 控制重复使用
+  read_template34(template_filename, cats, allow_repeat, &used_words);
+
+  // 释放内存
+  freeCatarray2(cats);
+  for (size_t i = 0; i < used_words.n_words; i++) {
+    free(used_words.words[i]);
   }
-
-  category_t used_words = {.words = NULL, .n_words = 0};
-  parse_template(story_file, cats, &used_words, no_reuse);
-  fclose(story_file);
-
-  free_category(&used_words);
-  free_catarray(cats);
+  free(used_words.words);
 
   return EXIT_SUCCESS;
 }
