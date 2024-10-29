16701e8fa78988dd44d839f7249d703b04f92aa2
sj445 <sj445@duke.edu>
2024-10-29 19:48:24 -0400

    63

--- 063_eval2/story-step4.c
+++ 063_eval2/story-step4.c
@@ -1,14 +1,11 @@
-// story-step4.c
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 #include "provided.h"
 #include "rand_story.h"
-
 int main(int argc, char ** argv) {
   int allow_repeat = 1;  // 默认允许重复
-
   // 解析命令行参数
   if (argc < 3 || argc > 4) {
     fprintf(stderr, "Usage: %s [-n] <words file> <story template>\n", argv[0]);
@@ -19,9 +16,10 @@ int main(int argc, char ** argv) {
   if (strcmp(argv[1], "-n") == 0) {
     allow_repeat = 0;  // 不允许重复
     argv++;            // 向后移动参数
+    argc--;            // 减少参数计数
   }
 
-  FILE * f = fopen(argv[1], "r");
+  FILE * f = fopen(argv[1], "r");  // 确保argv[1]是words文件
   if (f == NULL) {
     perror("Could not open words file");
     return EXIT_FAILURE;
@@ -29,9 +27,7 @@ int main(int argc, char ** argv) {
 
   catarray_t * cats = readWords2(f);
   fclose(f);
-
   read_template_with_backreference(argv[2], cats, allow_repeat);  // 使用允许重复参数
-
   freeCatarray2(cats);
   return EXIT_SUCCESS;
 }
