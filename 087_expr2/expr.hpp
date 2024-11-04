fd9b3b212bd12ec2a3ea4dd9c28d277ba1a702aa
sj445 <sj445@duke.edu>
2024-11-04 16:41:47 -0500

    87

---
+++ 087_expr2/expr.hpp
@@ -0,0 +1,81 @@
+#ifndef _EXPR_H_
+#define _EXPR_H_
+
+#include <sstream>
+#include <string>
+
+class Expression {
+ public:
+  virtual ~Expression() {}
+  virtual std::string toString() const = 0;
+};
+
+class NumExpression : public Expression {
+ private:
+  long number;
+
+ public:
+  NumExpression(long n) : number(n) {}
+  std::string toString() const {
+    std::stringstream result;
+    result << number;
+    return result.str();
+  }
+};
+
+class BinaryExpression : public Expression {
+ protected:
+  Expression * lhs;
+  Expression * rhs;
+
+ public:
+  BinaryExpression(Expression * left, Expression * right) : lhs(left), rhs(right) {}
+  virtual ~BinaryExpression() {
+    delete lhs;
+    delete rhs;
+  }
+};
+
+class PlusExpression : public BinaryExpression {
+ public:
+  PlusExpression(Expression * left, Expression * right) : BinaryExpression(left, right) {}
+  std::string toString() const {
+    std::stringstream result;
+    result << "(" << lhs->toString() << " + " << rhs->toString() << ")";
+    return result.str();
+  }
+};
+
+class MinusExpression : public BinaryExpression {
+ public:
+  MinusExpression(Expression * left, Expression * right) :
+      BinaryExpression(left, right) {}
+  std::string toString() const {
+    std::stringstream result;
+    result << "(" << lhs->toString() << " - " << rhs->toString() << ")";
+    return result.str();
+  }
+};
+
+class TimesExpression : public BinaryExpression {
+ public:
+  TimesExpression(Expression * left, Expression * right) :
+      BinaryExpression(left, right) {}
+  std::string toString() const {
+    std::stringstream result;
+    result << "(" << lhs->toString() << " * " << rhs->toString() << ")";
+    return result.str();
+  }
+};
+
+class DivExpression : public BinaryExpression {
+ public:
+  DivExpression(Expression * left, Expression * right) : BinaryExpression(left, right) {}
+  std::string toString() const {
+    std::stringstream result;
+    result << "(" << lhs->toString() << " / " << rhs->toString() << ")";
+    return result.str();
+  }
+};
+
+#endif
