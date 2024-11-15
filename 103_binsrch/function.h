b0ca7a54b95bc32f6b023b3331729930cdad2174
sj445 <sj445@duke.edu>
2024-11-14 21:31:05 -0500

    103

--- 103_binsrch/function.h
+++ 103_binsrch/function.h
@@ -1,9 +1,31 @@
 #ifndef __FUNCTION_H__
 #define __FUNCTION_H__
-template<typename R, typename A> 
+
+#include <cstdlib>
+#include <iostream>
+
+template<typename R, typename A>
 class Function {
-public:
+ public:
   virtual R invoke(A arg) = 0;
   virtual ~Function() {}
 };
+
+int binarySearchForZero(Function<int, int> * f, int low, int high) {
+  while (low < high - 1) {
+    int mid = low + (high - low) / 2;
+    int res = f->invoke(mid);
+
+    if (res == 0) {
+      return mid;
+    }
+    else if (res > 0) {
+      high = mid;
+    }
+    else {
+      low = mid;
+    }
+  }
+  return low;
+}
 #endif
