41c4bf2e135e0efbb8a2d5e91aff7249aafe6191
sj445 <sj445@duke.edu>
2024-12-05 17:07:59 -0500

    99

--- 099_eval3/step4.cpp
+++ 099_eval3/step4.cpp
@@ -101,31 +101,91 @@ void parse_ships(std::ifstream & file, std::vector<Ship> & ships) {
     else if (type_details.find("Tanker") != std::string::npos) {
       ship.type = "Tanker";
       std::stringstream type_stream(type_details);
-      std::string token;
-      std::vector<int> tank_values;
+      std::string tanks_str, last_value;
 
-      while (std::getline(type_stream, token, ',')) {
-        tank_values.push_back(std::atoi(token.c_str()));
+      std::getline(type_stream, ship.type, ',');  // 类型
+      while (std::getline(type_stream, last_value, ',')) {
+        tanks_str = last_value;  // 取最后一个值
       }
-      if (!tank_values.empty()) {
-        ship.tanks = tank_values.back();  // 取最后一个值作为tanks的值
-      }
-
-      std::string restrictions_str;
-      std::getline(type_stream, restrictions_str, ':');  // 限制条件
-      if (!restrictions_str.empty()) {
-        std::stringstream res_stream(restrictions_str);
-        std::string restriction;
-        while (std::getline(res_stream, restriction, ',')) {
-          ship.restrictions.push_back(restriction);
-        }
-      }
+      ship.tanks = std::atoi(tanks_str.c_str());
     }
 
     std::getline(ss, ship.origin, ':');       // 起点
     std::getline(ss, ship.destination, ':');  // 终点
     ss >> ship.capacity;                      // 容量
     ships.push_back(ship);
+  }
+}
+
+void parse_cargo(std::ifstream & file, std::vector<Cargo> & cargoes) {
+  std::string line;
+  while (std::getline(file, line)) {
+    std::stringstream ss(line);
+    Cargo cargo;
+    std::string weight_str, properties_str;
+
+    std::getline(ss, cargo.name, ',');
+    std::getline(ss, cargo.origin, ',');
+    std::getline(ss, cargo.destination, ',');
+    std::getline(ss, weight_str, ',');
+    cargo.weight = std::atoi(weight_str.c_str());
+    std::getline(ss, cargo.category, ',');
+    std::getline(ss, properties_str);
+    if (!properties_str.empty()) {
+      std::stringstream prop_ss(properties_str);
+      std::string property;
+      while (std::getline(prop_ss, property, ',')) {
+        cargo.properties.push_back(property);
+      }
+    }
+    cargoes.push_back(cargo);
+  }
+}
+
+void load_cargo(const std::vector<Cargo> & cargoes, std::vector<Ship> & ships) {
+  typedef AVLMultiMap<uint64_t, Ship *> ShipMap;
+  ShipMap ship_map;
+
+  for (size_t i = 0; i < ships.size(); i++) {
+    ship_map.add(ships[i].remaining_capacity(), &ships[i]);
+  }
+
+  for (size_t i = 0; i < cargoes.size(); i++) {
+    const Cargo & cargo = cargoes[i];
+    Ship * best_ship = NULL;
+
+    std::vector<std::pair<std::pair<uint64_t, std::set<Ship *> >, int> > ship_data =
+        ship_map.preOrderDump();
+
+    for (size_t j = 0; j < ship_data.size(); j++) {
+      const std::set<Ship *> & ship_set = ship_data[j].first.second;
+
+      for (std::set<Ship *>::iterator it = ship_set.begin(); it != ship_set.end(); ++it) {
+        Ship * ship = *it;
+        if (ship->can_load(cargo)) {
+          if (!best_ship ||
+              ship->remaining_capacity() < best_ship->remaining_capacity() ||
+              (ship->remaining_capacity() == best_ship->remaining_capacity() &&
+               ship->name < best_ship->name)) {
+            best_ship = ship;
+          }
+        }
+      }
+    }
+
+    if (best_ship) {
+      std::cout << "Loading " << cargo.name << " onto " << best_ship->name << " from "
+                << cargo.origin << " to " << cargo.destination << " "
+                << best_ship->remaining_capacity() - cargo.weight
+                << " capacity remains\n";
+      ship_map.remove(best_ship->remaining_capacity(), best_ship);
+      best_ship->load_cargo(cargo);
+      ship_map.add(best_ship->remaining_capacity(), best_ship);
+    }
+    else {
+      std::cout << "No ships can carry the " << cargo.name << " from " << cargo.origin
+                << " to " << cargo.destination << "\n";
+    }
   }
 }
 
@@ -165,7 +225,10 @@ int main(int argc, char ** argv) {
   std::vector<Ship> ships;
   std::vector<Cargo> cargoes;
   parse_ships(ships_file, ships);
+  parse_cargo(cargo_file, cargoes);
 
+  std::sort(cargoes.begin(), cargoes.end(), cargo_weight_desc);
+  load_cargo(cargoes, ships);
   print_ships(ships);
 
   return EXIT_SUCCESS;
